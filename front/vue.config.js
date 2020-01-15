b1ff65a24aafae55c52ff13478d7128fa605acab
GZennurov <zwgybds@yahoo.com>
2020-01-15 17:14:36 +0300

    Fixing vue.config.js for production deploy

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,8 +1,12 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? 'online-conference'
+        : '',
+
     devServer: {
         proxy: {
             '': {
-                target: 'http://localhost:4000',
+                target: 'https://online-conference-heroku.herokuapp.com/',
                 secure: false,
                 changeOrigin: true,
             },
