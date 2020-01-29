ab52ed388c593901127dab80ddee80c25d244bbc
GZennurov <zwgybds@yahoo.com>
2020-01-29 09:06:35 +0300

    Added loading indicator

--- front/src/chat/chatMain.vue
+++ front/src/chat/chatMain.vue
@@ -41,6 +41,9 @@
                 @entered="onEntered"
                 @move-creation="onCreationOpen"
         />
+        <div v-if="isLoading" class="loading">
+            <div  class="loading__circle"></div>
+        </div>
     </form>
 </template>
 
@@ -65,6 +68,8 @@ import { SocketService } from "@/services/SocketService";
 export default class chatMain extends Vue {
     isCreationOpen: boolean = false;
     isEnterOpen: boolean = false;
+    isLoading: boolean = true;
+
     conferences: string [] = [];
     inputMessage: string = '';
     socketService: SocketService | null = null;
@@ -72,23 +77,27 @@ export default class chatMain extends Vue {
     messages: IMessage [] = [];
     participants: IParticipant [] = [];
 
-    sendMessage(message: string){
+    sendMessage(message: string) {
         this.socketService && this.socketService.sendMessage(message);
         this.inputMessage = '';
     }
 
-    async onConfCreation(creadentials: any){
+    async onConfCreation(creadentials: any) {
+        this.isCreationOpen = false;
+        this.isLoading = true;
         const { login, confName } = creadentials;
         const participant = <IParticipant> { login }
         this.confName = await (<any> this.socketService).createConference(participant, confName);
-        this.isCreationOpen = false;
+        this.isLoading = false;
     }
 
-    async onEntered(creadentials: any){
+    async onEntered(creadentials: any) {
+        this.isEnterOpen = false;
+        this.isLoading = true;
         const { login, confName } = creadentials;
         const participant = <IParticipant> { login };
         this.confName = await (<any> this.socketService).joinConf(participant, confName);
-        this.isEnterOpen = false;
+        this.isLoading = false;
     }
 
     onCreationOpen(){
@@ -96,8 +105,8 @@ export default class chatMain extends Vue {
         this.isCreationOpen = true;
     }
 
-    onDisconnect(){
-        console.log("disconnected")
+    onDisconnect() {
+        console.log("disconnected");
         window.location.reload(false);
     }
 
@@ -114,6 +123,7 @@ export default class chatMain extends Vue {
         this.socketService.disconnectCallBack = this.onDisconnect;
         this.socketService.getConferences().then( res => {
             this.conferences = res;
+            this.isLoading = false;
             this.openModal();
         });
     }
@@ -123,89 +133,118 @@ export default class chatMain extends Vue {
 
 <style lang="scss">
 
-.chat {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    background-color: whitesmoke;
-    padding: 30px 40px;
-
-    &__header{
+    .chat {
+        width: 100%;
+        height: 100%;
         display: flex;
-        align-items: center;
-        padding: 25px 0;
-        border-bottom: 2px solid gainsboro;
+        flex-direction: column;
+        background-color: whitesmoke;
+        padding: 30px 40px;
 
-        &-chatname{
+        &__header{
             display: flex;
             align-items: center;
-            font-weight: bold;
-            font-size: 18px;
+            padding: 25px 0;
+            border-bottom: 2px solid gainsboro;
+
+            &-chatname{
+                display: flex;
+                align-items: center;
+                font-weight: bold;
+                font-size: 18px;
+            }
+
+            &-online{
+                margin-left: 15px;
+                height: 12px;
+                width: 12px ;
+                background: url("../assets/online@1X.png") no-repeat;
+                background-size: contain;
+                background-position: center;
+                opacity: 0.5;
+            }
+
+            &-menu{
+                margin-left: auto;
+                z-index: 2;
+            }
         }
 
-        &-online{
-            margin-left: 15px; 
-            height: 12px;
-            width: 12px ;
-            background: url("../assets/online@1X.png") no-repeat;
-            background-size: contain;
-            background-position: center;
-            opacity: 0.5;
+        &__messages {
+            overflow-y: hidden;
         }
 
-        &-menu{
-            margin-left: auto;
-            z-index: 2;
-        }      
-    }
-
-    &__messages {
-        overflow-y: hidden;
-    }
-
-    &__send{
-        margin-top: auto;
-        display: flex;
-        align-items: center;
-        position: relative;
-        background-color: white;
-        box-shadow: 10px 5px 20px  #BEBEBE;
-
-        &-input {
-            border : none;
-            padding: 35px;
-            font-size: 16px;
+        &__send{
+            margin-top: auto;
+            display: flex;
+            align-items: center;
+            position: relative;
             background-color: white;
-            flex: 1;
+            box-shadow: 10px 5px 20px  #BEBEBE;
 
-            &:focus {
-                outline: none;
-            }
-        }
+            &-input {
+                border : none;
+                padding: 35px;
+                font-size: 16px;
+                background-color: white;
+                flex: 1;
 
-        &-button{
-            border : none;
-            outline: inherit;
-            border-radius: 50%;
-            width: 55px;
-            height: 55px;
-            margin-right: 20px;
-            background: url("../assets/Layer 13@1X.png") no-repeat;
-            background-position: center;
-            background-size: 2rem;
-            background-color: #89CFF0;
-            box-shadow:  5px 5px 20px  #BEBEBE;
-
-            &:hover{
-                cursor: pointer;
+                &:focus {
+                    outline: none;
+                }
             }
 
-            &:active{
-                opacity: 0.7;
+            &-button{
+                border : none;
+                outline: inherit;
+                border-radius: 50%;
+                width: 55px;
+                height: 55px;
+                margin-right: 20px;
+                background: url("../assets/Layer 13@1X.png") no-repeat;
+                background-position: center;
+                background-size: 2rem;
+                background-color: #89CFF0;
+                box-shadow:  5px 5px 20px  #BEBEBE;
+
+                &:hover{
+                    cursor: pointer;
+                }
+
+                &:active{
+                    opacity: 0.7;
+                }
             }
         }
     }
-}
+
+    .loading {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 2;
+        background-color: rgba(0,0,0,0.4);
+
+        &__circle {
+            width: 2rem;
+            height: 2rem;
+            border-radius: 50%;
+            border: 0.3rem solid rgba(#89CFF0, 0.3);
+            border-top-color: #89CFF0;
+            animation: 1.5s spin infinite linear;
+        }
+    }
+
+
+    @keyframes spin {
+        to {
+            transform: rotate(360deg);
+        }
+    }
 
 </style>
